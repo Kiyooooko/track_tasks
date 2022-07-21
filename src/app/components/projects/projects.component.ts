c5fd4e0010be358dc681594085766ecd2747e98e
volodymyrpankiv <volodymyr.pankiv@teck.com>
2022-07-21 11:48:01 +0300

    added CRUD with angular/fire

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Database, objectVal, ref, set, update, remove } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
+interface Project  {
+  title: string;
+  id: string;
+}
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent {
+  public readonly projects$: Observable<Project[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private database: Database) {
+    const doc = ref(database, 'projects');
+    this.projects$ = objectVal(doc).pipe(map((res: any) => Object.values(res)));
   }
 
+  addProject(id = 'new') {
+    set(ref(this.database, `projects/${id}`), {
+      id,
+      title: 'super puper'
+    })
+  }
+
+  updateProject(id = 'new') {
+    const postData = {
+      title: 'New title3',
+      id: 'lala'
+    };
+
+    const newKey = `/projects/new/`
+    const updates: { [newKey]: Project} = {
+      [newKey]: postData
+    };
+
+    update(ref(this.database), updates);
+  }
+
+  deleteProject(id: string) {
+    const itemToRemove = ref(this.database, `projects/${id}`)
+    remove(itemToRemove);
+  }
 }
