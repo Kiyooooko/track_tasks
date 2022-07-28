ff0852f883482cd6728895ad115a05ee95ca2924
Olena <olena.shchadylo@ukr.net>
2022-07-28 15:04:25 +0300

    test

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -2,54 +2,51 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-    config.set({
-      basePath: '',
-      frameworks: ['jasmine', '@angular-devkit/build-angular'],
-      customLaunchers: {
-        ChromeHeadless: {
-          base: 'Chrome',
-          flags: [
-            '--headless',
-            '--disable-gpu',
-            '--remote-debugging-port=9222',
-            '--no-sandbox'
-          ]
-        }
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222',
+          '--no-sandbox',
+        ],
       },
-      plugins: [
-        require('karma-jasmine'),
-        require('karma-chrome-launcher'),
-        require('karma-jasmine-html-reporter'),
-        require('karma-coverage'),
-        require('@angular-devkit/build-angular/plugins/karma')
-      ],
-      client: {
-        jasmine: {
-          // you can add configuration options for Jasmine here
-          // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-          // for example, you can disable the random execution with `random: false`
-          // or set a specific seed with `seed: 4321`
-        },
-        clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
       },
-      jasmineHtmlReporter: {
-        suppressAll: true // removes the duplicated traces
-      },
-      coverageReporter: {
-        dir: require('path').join(__dirname, './coverage/task_tracking'),
-        subdir: '.',
-        reporters: [
-          { type: 'html' },
-          { type: 'text-summary' }
-        ]
-      },
-      reporters: ['progress', 'kjhtml'],
-      port: 9876,
-      colors: true,
-      logLevel: config.LOG_INFO,
-      autoWatch: false,
-      browsers: ['ChromeHeadless'],
-      singleRun: true,
-      restartOnFileChange: true
-    });
-  };
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/task_tracking'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: true,
+  });
+};
